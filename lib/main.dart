e6040e03b3a64ad675a0078b19c649fc54e4b5f0
Jumzeey <31732947+Jumzeey@users.noreply.github.com>
2024-06-30 15:16:26 +0100

    added reset quiz button and dynamic result with getters

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:first_app/quiz.dart';
+import 'package:first_app/result.dart';
 import 'package:flutter/material.dart';
 
 // void main() {
@@ -16,23 +17,38 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
   final List<Map<String, Object>> _questions = [
     {
       'questionText': 'What is your name?',
-      'answers': ['John', 'Doe', 'Smith'],
+      'answers': [
+        {'text': 'John', 'score': 1},
+        {'text': 'Doe', 'score': 1},
+        {'text': 'Smith', 'score': 1}
+      ],
     },
     {
       'questionText': 'What is your favorite color?',
-      'answers': ['Red', 'Blue', 'Green', 'Black'],
+      'answers': [
+        {'text': 'Red', 'score': 6},
+        {'text': 'Blue', 'score': 1},
+        {'text': 'Green', 'score': 4},
+        {'text': 'Black', 'score': 10}
+      ],
     },
     {
       'questionText': 'What is your hobby?',
-      'answers': ['Reading', 'Writing', 'Hiking'],
+      'answers': [
+        {'text': 'Reading', 'score': 10},
+        {'text': 'Writing', 'score': 5},
+        {'text': 'Hiking', 'score': 1}
+      ],
     },
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex++;
     });
@@ -44,9 +60,15 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -54,9 +76,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: _questionIndex < _questions.length
             ? Quiz(_answerQuestion, _questions, _questionIndex)
-            : const Center(
-                child: Text('You did it!'),
-              ),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,21 +4,20 @@ import 'package:flutter/material.dart';
 
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
-  final VoidCallback answerQuestion;
+  final Function answerQuestion;
   final int questionIndex;
 
-  const Quiz(this.answerQuestion, this.questions, this.questionIndex, {super.key});
+  const Quiz(this.answerQuestion, this.questions, this.questionIndex,
+      {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return 
-    Column(
+    return Column(
       children: [
-        Question(
-            questions[questionIndex]['questionText'] as String),
-        ...(questions[questionIndex]['answers'] as List<String>)
+        Question(questions[questionIndex]['questionText'] as String),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
             .map((answer) {
-          return Answer(answerQuestion, answer);
+          return Answer(() => answerQuestion(answer['score'] as int), answer['text'] as String);
         }),
       ],
     );
